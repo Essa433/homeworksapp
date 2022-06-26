fc7ec62f197e0613f07843c1ddbed4916287fd64
Essa <essa1417323@gmail.com>
2022-06-27 01:04:36 +0300

    first commit

--- src/routes/homeworks.ts
+++ src/routes/homeworks.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from 'fastify';
 
 export default async function (server: FastifyInstance) {
-	server.get('/', async (request, reply) => {
+	server.get('/essa', async (request: any, reply: any) => {
 		// add your code here
 	});
 
-	server.get('/Homework', async (request, reply) => {
+	server.get('/essa/Homework', async (request: any, reply: any) => {
 		// add your code here
 	});
 }
